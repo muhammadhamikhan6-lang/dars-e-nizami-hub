533a4ceac30455b4ee094db974f5948d6759946b
muhammadhamikhan6-lang <muhammadhamikhan6@gmail.com>
2026-01-11 12:57:52 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function showResult() {
+    document.getElementById("result").innerHTML = `
+        <h2>Available Content</h2>
+        <p>Select purchase type:</p>
+
+        <button>📦 Buy Physical Book</button><br><br>
+        <button>💻 Buy Online (Secure)</button>
+    `;
+}
